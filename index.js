af9cba0845ff04ec2e188eb2735ccada6f77f5ab
anvaka <anvaka@gmail.com>
2015-07-01 23:16:18 -0700

    Slide_1

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ var particlesCount = 1000;
 var particleView = require('./lib/createParticleView.js')(particlesCount);
 var particles = require('./lib/createParticleObject.js')(particlesCount);
 
-var target = {x: 0, y: 0};
-
-document.body.addEventListener('mousemove', setNewTarget, true);
-document.body.addEventListener('mousedown', boom, true);
-
 frame();
 function frame() {
   requestAnimationFrame(frame);
@@ -19,23 +14,10 @@ function renderParticles() {
   for (var i = 0; i < particlesCount; ++i) {
     var p = particles[i];
 
-    p.moveTo(target.x, target.y);
-
     coordinates[i * 3] = p.x;
     coordinates[i * 3 + 1] = p.y;
     coordinates[i * 3 + 2] = 0;
   }
 
   particleView.coordinates(coordinates);
-}
-
-function setNewTarget(e) {
-  target = particleView.getCoordinates(e.clientX, e.clientY);
-}
-
-function boom() {
-  for (var i = 0; i < particlesCount; ++i) {
-    var p = particles[i];
-    p.boom();
-  }
 }
